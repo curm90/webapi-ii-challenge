576a15c3317579020c0885cdf2adfddd35a917bf
Liam Sutton <devliam90@gmail.com>
2019-10-29 12:50:08 +0000

    add get all posts request

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
+const db = require('./data/db/db');
 const port = process.env.PORT || 3300;
 const server = express();
 
 server.get('/', (req, res) => {
   res.send('hello world');
+});
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved' });
+    });
 });
 
 server.listen(port, () => {
