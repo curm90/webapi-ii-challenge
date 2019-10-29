e3a5797691ba5fe601228711d987e0803c7e1fb1
Liam Sutton <devliam90@gmail.com>
2019-10-29 13:09:16 +0000

    add get post by id response

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ server.get('/api/posts', (req, res) => {
     });
 });
 
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(post => {
+      if (post.length > 0) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
