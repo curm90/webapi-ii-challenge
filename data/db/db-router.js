06f5da7c246f8d939f225cb899bf08786cd59fb7
Liam Sutton <devliam90@gmail.com>
2019-10-29 15:00:59 +0000

    add update request

--- data/db/db-router.js
+++ data/db/db-router.js
@@ -4,16 +4,27 @@ const db = require('./db');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  const post = req.body;
-  db.insert(post)
-    .then(data => {
-      console.log(data);
-    })
-    .catch(() => {
-      res.status(500).json({
-        error: 'There was an error while saving the post to the database'
-      });
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
     });
+  } else {
+    db.insert({ title, contents })
+      .then(data => {
+        res.status(201).json({
+          id: data.id,
+          title,
+          contents
+        });
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: 'There was an error while saving the post to the database'
+        });
+      });
+  }
 });
 
 router.get('/', (req, res) => {
@@ -30,6 +41,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
+
   db.findById(id)
     .then(post => {
       if (post.length > 0) {
@@ -44,6 +56,52 @@ router.get('/:id', (req, res) => {
       res
         .status(500)
         .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+// router.delete('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const postToDelete = db.findById(id);
+
+//   db.remove(id)
+//     .then(post => {
+//       if (post > 0) {
+//         res.status(200).json(postToDelete);
+//       } else {
+//         res
+//           .status(404)
+//           .json({ message: 'The post with the specified ID does not exist.' });
+//       }
+//     })
+//     .catch(() => {
+//       res.status(500).json({ error: 'The post could not be removed' });
+//     });
+// });
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const postToUpdate = req.body;
+
+  db.update(id, postToUpdate)
+    .then(post => {
+      if (post > 0) {
+        res.status(200).json(postToUpdate);
+      } else if (!postToUpdate.title || !postToUpdate.contents) {
+        res
+          .status(400)
+          .json({
+            errorMessage: 'Please provide title and contents for the post.'
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' });
     });
 });
 
