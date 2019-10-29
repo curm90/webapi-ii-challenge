f6ce7c683d0e8e187570101f983fc9fc7de4d2b6
Liam Sutton <devliam90@gmail.com>
2019-10-29 17:38:34 +0000

    MVP done

--- data/db/db-router.js
+++ data/db/db-router.js
@@ -59,39 +59,57 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// router.delete('/:id', (req, res) => {
-//   const { id } = req.params;
-//   const postToDelete = db.findById(id);
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
 
-//   db.remove(id)
-//     .then(post => {
-//       if (post > 0) {
-//         res.status(200).json(postToDelete);
-//       } else {
-//         res
-//           .status(404)
-//           .json({ message: 'The post with the specified ID does not exist.' });
-//       }
-//     })
-//     .catch(() => {
-//       res.status(500).json({ error: 'The post could not be removed' });
-//     });
-// });
+  db.remove(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json({ message: 'Post was deleted successfully' });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'The post could not be removed.' });
+    });
+});
 
 router.put('/:id', (req, res) => {
   const { id } = req.params;
-  const postToUpdate = req.body;
+  const { title, contents } = req.body;
 
-  db.update(id, postToUpdate)
-    .then(post => {
-      if (post > 0) {
-        res.status(200).json(postToUpdate);
-      } else if (!postToUpdate.title || !postToUpdate.contents) {
-        res
-          .status(400)
-          .json({
-            errorMessage: 'Please provide title and contents for the post.'
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
+    });
+  } else {
+    db.update(id, { title, contents })
+      .then(post => {
+        if (post > 0) {
+          res.status(200).json({ ...post, title, contents });
+        } else {
+          res.status(404).json({
+            message: 'The post with the specified ID does not exist.'
           });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: 'The post information could not be modified.' });
+      });
+  }
+});
+
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+  db.findPostComments(id)
+    .then(postComments => {
+      if (postComments) {
+        res.status(200).json(postComments);
       } else {
         res
           .status(404)
@@ -101,7 +119,34 @@ router.put('/:id', (req, res) => {
     .catch(() => {
       res
         .status(500)
-        .json({ error: 'The post information could not be modified.' });
+        .json({ error: 'The comments information could not be retrieved.' });
+    });
+});
+
+router.post('/:id/comments', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+
+  db.findById(id)
+    .then(post => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      } else if (!text) {
+        res
+          .status(400)
+          .json({ errorMessage: 'Please provide text for the comment.' });
+      } else {
+        db.insertComment({ text, post_id: id }).then(comment => {
+          res.status(201).json(comment);
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: 'There was an error while saving the comment to the database'
+      });
     });
 });
 
